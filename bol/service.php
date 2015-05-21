bf7113f9e67f44942241c366c5d5e964b4841f73
Thao Le <thaolt@songphi.com>
2015-05-21 08:55:18 +0700

    marked as v1.0.2;

--- bol/service.php
+++ bol/service.php
@@ -28,7 +28,7 @@
 class SPSEO_BOL_Service
 {
     const PLUGIN_NAME = 'spseo';
-    const PLUGIN_VER = 'v1.0.1';
+    const PLUGIN_VER = 'v1.0.2';
     protected static $classInstance = null;
 
     private $bridges;
